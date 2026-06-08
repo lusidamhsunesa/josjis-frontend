97c80433058318d7a9537c04094af4fa1c64ceed
bluesky4047 <161824608+bluesky4047@users.noreply.github.com>
2026-06-08 15:58:16 +0700

    feat(vite config): add dinamic port

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,26 @@
-import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig, loadEnv } from "vite";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import babel from "@rolldown/plugin-babel";
+import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] }),
-    tailwindcss()
-  ],
-})
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  const port = Number(env.VITE_PORT) || 5173;
+
+  return {
+    plugins: [
+      react(),
+      babel({ presets: [reactCompilerPreset()] }),
+      tailwindcss(),
+    ],
+    server: {
+      port,
+    },
+    preview: {
+      port,
+      allowedHosts: ["josjis.rafn.tech"],
+    },
+  };
+});
