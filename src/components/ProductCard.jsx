e0089b5e733ca6827574d29d066fe42a8a75b811
lucida chintya <lusidachintya.com>
2026-06-09 22:44:18 +0700

    Update product card styling, button color, hover

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -16,7 +16,7 @@ const ProductCard = ({ product }) => {
   return (
     <div
       onClick={() => navigate(`/product/${product.id}`)}
-      className="bg-white/25 backdrop-blur-lg border border-white/30 rounded-2xl overflow-hidden shadow-[0_8px_32px_0_rgba(0,0,0,0.3)] flex flex-col relative group transition-all hover:shadow-[0_8px_32px_0_rgba(0,0,0,0.5)] cursor-pointer h-full"
+      className="bg-white/1 backdrop-blur-lg border border-white/40 rounded-2xl overflow-hidden shadow-[0_8px_32px_0_rgba(0,0,0,0.3)] flex flex-col relative group transition-all hover:shadow-[0_8px_32px_0_rgba(0,0,0,0.5)] cursor-pointer h-full"
     >
       <div className="h-[280px] w-full overflow-hidden relative border-b border-white/20">
         <img
@@ -32,16 +32,16 @@ const ProductCard = ({ product }) => {
       </div>
 
       <div className="p-5 flex flex-col gap-2  bg-white/10">
-        <h3 className="font-roboto font-black text-xl text-black truncate drop-shadow-sm">
+        <h3 className="font-roboto font-black text-xl text-white truncate drop-shadow-sm">
           {product.name}
         </h3>
-        <p className="font-paytone text-2xl text-black">
+        <p className="font-paytone text-2xl text-white">
           {rupiahFormat(product.price)}
         </p>
 
         <button
           onClick={handleAddToCart}
-          className="absolute bottom-5 right-5 bg-success-green flex items-center gap-1 px-4 py-2 rounded-lg text-black font-roboto font-black text-xs hover:bg-green-500 transition-colors shadow-lg active:scale-95"
+          className="absolute bottom-5 right-5 bg-accent-yellow flex items-center gap-1 px-4 py-2 rounded-lg text-brown font-roboto font-black text-xs hover:bg-brown transition-colors shadow-lg active:scale-95 hover:text-accent-yellow"
         >
           <Plus size={16} />
           Tambah
